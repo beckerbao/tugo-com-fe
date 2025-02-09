8cbb1b39a7422e44d1a48c911df77aa81783db02
bao.nguyen <bao.nguyen@kyanon.digital>
2025-02-09 16:05:39 +0700

    test thử redirect

--- pages/reviewbyqr.php
+++ pages/reviewbyqr.php
@@ -112,7 +112,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         </div>
 
         <div class="form-group">
-            <label for="guest-phone">Số điện thoại khách</label>
+            <label for="guest-phone">Số điện thoại  (có sử dụng Zalo)</label>
             <input type="text" id="guest-phone" name="guest_phone" placeholder="Nhập số điện thoại" required>
         </div>
 
@@ -133,3 +133,44 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </div>
 <?php include '../includes/navbar.php'; ?>
 <?php include '../includes/footer.php'; ?>
+<script>
+    //goi ham redirectToDeviceBrowser sau khi đã load xong
+    window.addEventListener('load', redirectToDeviceBrowser);
+    
+    function redirectToDeviceBrowser(options = {}) {
+        const extraPath = options.extraPath || '';
+        const isServerEndPoint = options.isServerEndPoint || false;
+
+        // Lấy API Endpoint từ meta hoặc window.location
+        const endpoint = document.querySelector('meta[name="api-endpoint"]')?.getAttribute("content") || window.location.origin;
+        
+        // Xây dựng URL
+        const url = `${isServerEndPoint ? endpoint : window.location.origin}${extraPath}`;
+
+        // Kiểm tra thiết bị
+        const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+        // iOS
+        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+            setTimeout(() => { window.location.href = `x-safari-${url}`; }, 0);
+            // setTimeout(() => { window.location.href = `com-apple-mobilesafari-tab:${url}`; }, 1000);
+            setTimeout(() => { window.location.href = `googlechrome://${url.replace(/^https?:\/\//, '')}`; }, 2000);
+            setTimeout(() => { window.location.href = `firefox://open-url?url=${url}`; }, 3000);
+            setTimeout(() => { window.location.href = `x-web-search://?cicd.aitracuuluat.vn`; }, 4000);
+            return false;
+        }
+
+        // Android
+        if (/android/i.test(userAgent)) {
+            setTimeout(() => { window.location.href = `intent://${url.replace(/^https?:\/\//, '')}#Intent;scheme=https;package=com.android.chrome;end;`; }, 0);
+            setTimeout(() => { window.location.href = `googlechrome://navigate?url=${url}`; }, 1000);
+            setTimeout(() => { window.location.href = `firefox://open-url?url=${url}`; }, 2000);
+            return false;
+        }
+
+        // Mở trang web bình thường nếu không phải iOS/Android
+        window.location.href = url;
+        return true;
+    }
+
+</script>
