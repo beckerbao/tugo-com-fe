95424a0348f0d071e3c23b9b645d4c6a6958c368
bao.nguyen <bao.nguyen@kyanon.digital>
2025-02-09 15:06:27 +0700

    test

--- pages/agent.php
+++ pages/agent.php
@@ -2,8 +2,12 @@
 //detect user agent và hiển thị trên trang agent.php
 $user_agent = $_SERVER['HTTP_USER_AGENT'];
 
-echo "User Agent: $user_agent";
+// echo "User Agent: $user_agent";
+
+//nếu user sử dụng iOS thì mở bằng safari, nếu sử dụng Android thì mở bằng chrome
+if (strpos($user_agent, 'iPhone') !== false || strpos($user_agent, 'iPad') !== false || strpos($user_agent, 'iPod') !== false) {
+    header('Location: https://safari.com');
+} elseif (strpos($user_agent, 'Android') !== false) {
+    header('Location: https://chrome.com');
+}
 ?>
-<script>
-    window.open("http://localhost:8081/", '_system');
-</script>
