d5f3fe8e6c04e89edf5c089128693d5ed94602f0
bao.nguyen <bao.nguyen@kyanon.digital>
2025-03-18 16:23:01 +0700

    review dịch vụ sau 18 days

---
+++ pages/review18days.php
@@ -0,0 +1,27 @@
+<?php
+// Bắt đầu session
+session_start();
+$page_title = "Write a Review for Sale";
+include '../helpers/common.php';
+include '../includes/header.php';
+include '../helpers/apiCaller.php';
+
+APICaller::init();
+
+?>
+
+<link rel="stylesheet" href="../assets/css/review.css?<?php echo $version; ?>">
+<div class="review-container">
+    <!-- Phần nhúng Google Form -->
+    <section class="google-form">
+        <h2>Phản hồi đánh giá</h2>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeN-wiPpjcwwS5nNCRt5BeHIXOHZ6ZxlmFDAZUheLNMh9lxcA/viewform?embedded=true" 
+                width="640" height="1952" frameborder="0" marginheight="0" marginwidth="0">
+            Loading…
+        </iframe>
+    </section>
+    <div class="spacer"></div>
+</div>
+
+<?php include '../includes/navbar.php'; ?>
+<?php include '../includes/footer.php'; ?>
